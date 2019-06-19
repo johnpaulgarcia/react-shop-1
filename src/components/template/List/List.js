dda7dae5f900d257321e1c7e4336c2e80083dfbc
John Paul Garcia <37685580+johnpaulgarcia@users.noreply.github.com>
2019-06-19 17:19:45 +0800

    Update List.js

--- src/components/template/List/List.js
+++ src/components/template/List/List.js
@@ -1,8 +1,9 @@
 import React from 'react';
 const List = ({children}) => {
+    let class1 = `${toggle ? 'banana' : 'list-container'}`;
     return (
 
-        <div class="list-container">
+        <div class={class1}>
             {children}
         </div>
 
